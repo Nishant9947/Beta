21622b81121516584a3931354ec4b524ea589ac6
Nishant <82361039+Nishant9947@users.noreply.github.com>
2025-05-18 09:59:10 +0530

    Create projects.js

---
+++ projects.js
@@ -0,0 +1,24 @@
+// projects.js
+const projects = [
+    {
+        title: "Snake Game",
+        description: "Classic Snake game implementation using JavaScript",
+        tech: ["JavaScript", "HTML5", "CSS3"],
+        link: "https://github.com/Nishant9947/Snake-Game",
+        image: "assets/images/snake-game.jpg"
+    },
+    {
+        title: "To-Do List App",
+        description: "Interactive task management application",
+        tech: ["React", "Node.js", "MongoDB"],
+        link: "https://github.com/Nishant9947/To-Do-List",
+        image: "assets/images/todo-app.jpg"
+    },
+    {
+        title: "Calculator App",
+        description: "React-based calculator with scientific functions",
+        tech: ["React", "JavaScript", "CSS3"],
+        link: "https://github.com/Nishant9947/Calculator-App",
+        image: "assets/images/calculator.jpg"
+    }
+];
